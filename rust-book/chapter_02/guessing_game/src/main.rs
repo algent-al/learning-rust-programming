2575c8b1d5480096170abb9d180020509a54a75b
Algent Albrahimi <algent@protonmail.com>
2021-11-01 19:12:42 +0100

    chapter 02

---
+++ rust-book/chapter_02/guessing_game/src/main.rs
@@ -0,0 +1,45 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess the numbers!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    // println!("The secret number is: {}", secret_number);
+    loop {
+
+        println!("Please input your guess");
+
+        /* Get input in terminal from the user. 
+        "mut" allow this file to be modified */
+        let mut guess = String::new();
+
+        io::stdin()
+            // guess should be 'mut' to get a value
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        // Convert "guess" to a "u32" number type
+        // "trim()" will eliminate any whitespace at the beginning and end
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please input a number!");
+                continue;
+            },
+        };
+    
+        println!("You guessed {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
